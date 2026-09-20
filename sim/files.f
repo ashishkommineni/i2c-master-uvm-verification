6d2a8bcd38aa391c09103e38adb6aa4541eb0044
kommineni ashish <156738135+ashishkommineni@users.noreply.github.com>
2026-09-20 22:56:12 +0530

    Add Xcelium compilation file list

---
+++ sim/files.f
@@ -0,0 +1,8 @@
++incdir+tb/interfaces
++incdir+tb/pkg
+rtl/i2c_master_write.sv
+tb/interfaces/i2c_if.sv
+tb/models/i2c_slave_model.sv
+tb/assertions/i2c_sva.sv
+tb/pkg/i2c_uvm_pkg.sv
+tb/top/tb_top.sv
